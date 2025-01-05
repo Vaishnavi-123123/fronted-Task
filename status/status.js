07143e552918e77e6a25e0f65d603923112a359f
unknown <banaitvaishnavi6@gmail.com>
2025-01-05 18:27:47 +0530

    first commit

---
+++ status/status.js
@@ -0,0 +1,128 @@
+var contect =[
+    {
+        fullName: "Amit Sharma",
+        profilePhoto: "https://images.unsplash.com/photo-1676574834058-1d5717a3c04a?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1733860532230-37fa583a57a0?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        timeUpdated: "Yesterday"
+    },
+    {
+        
+        fullName: "Priya Singh",
+        profilePhoto: "https://images.unsplash.com/photo-1598892569729-adea55d207d8?q=80&w=1775&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1731484395148-b1d7f2af7d54?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        timeUpdated: "10 minutes ago"
+    },
+    {
+        fullName: "Harshdeep singh",
+        profilePhoto: " https://images.unsplash.com/photo-1715223298972-35c9609941a9?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1699639108878-febc7c35c374?q=80&w=1885&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        timeUpdated: "4:50 pm"
+    },
+    {
+        fullName: "Sneha Patel",
+        profilePhoto: "https://plus.unsplash.com/premium_photo-1732893654722-5ee56c538fc7?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D ",
+        story: "https://images.unsplash.com/photo-1445820200644-69f87d946277?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        timeUpdated: "1 hour ago"
+    },
+    {
+        fullName: "Rohit Mehta",
+        profilePhoto: "https://plus.unsplash.com/premium_photo-1683751606316-b618ae8829dc?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D ",
+        story: "https://plus.unsplash.com/premium_photo-1727552116095-889cb485a803?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        timeUpdated: "3:00 am "
+    },
+    {
+        fullName: "Neha Gupta",
+        profilePhoto: "https://images.unsplash.com/photo-1733737272425-c7455b65e29f?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D ",
+        story: "https://plus.unsplash.com/premium_photo-1664367173144-7e854e199524?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        timeUpdated: "45 minutes ago"
+    },
+    {
+        fullName: "Arjun Rao",
+        profilePhoto: " https://images.unsplash.com/photo-1542681466709-e7d1bd50df70?q=80&w=1912&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        story: "https://images.unsplash.com/photo-1729780922545-c234e33ad751?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        timeUpdated: "1:45 pm",
+    },
+    {
+        fullName: "Meera Iyer",
+        profilePhoto: " https://images.unsplash.com/photo-1659293554631-d7a38642c5e3?q=80&w=1772&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D ",
+        story: "https://images.unsplash.com/photo-1733227152851-908b8dfe6602?q=80&w=1886&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        timeUpdated: "20 minutes ago"
+    },
+    {
+        fullName: "Vikram Desai",
+        profilePhoto: "https://images.unsplash.com/photo-1511632765486-a01980e01a18?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D ",
+        story: "https://images.unsplash.com/photo-1577201866982-97ac098537e5?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        timeUpdated: "Yesterday"
+    },
+    {
+        fullName: "Ananya Roy",
+        profilePhoto: "https://images.unsplash.com/photo-1514985770-1ce286918a3b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D ",
+        story: "https://plus.unsplash.com/premium_photo-1676667573109-273586405c96?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        timeUpdated: "7:33 am"
+    }
+]
+
+
+var sum = ''
+
+contect.forEach(function(elem , idx){
+    
+    sum += `<div class="overlay"  id = ${idx}>
+                <img src="${elem.profilePhoto}",
+                alt="status">
+             <div id="contect">
+                <h3>${elem.fullName}</h3>
+                <h6>${elem.timeUpdated}</h6>
+             </div>
+                
+            </div>`
+
+
+            
+            
+
+
+
+            
+})
+
+
+var allstatus = document.querySelector('#allstatus')
+var full = document.querySelector('#full')
+var growth = document.querySelector('#growth')
+
+allstatus.innerHTML = sum
+
+allstatus.addEventListener('click', function(dets){
+    var gold = contect[dets.target.id];
+    var grow = 0;
+    const inter = setInterval(() => {
+        growth.style.width = grow + '%'
+        grow++;
+
+    }, 30);
+
+
+    
+    full.style.display = 'block'
+    full.style.backgroundImage = `url(${gold.story})`
+
+    setTimeout(() => {
+        full.style.display = 'none'
+        clearInterval(inter)
+    }, 3000);
+
+    
+    
+    
+})
+
+ 
+    
+
+
+ 
+
+
+
+
